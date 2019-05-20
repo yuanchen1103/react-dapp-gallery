0b286bab555f98d80cf66e6d3b511c9839ea8fde
Evis Cheng <e920528@gmail.com>
2019-05-20 23:46:14 +0800

    成功拿回來啦啦啦啦 (｡◕∀◕｡)

--- contracts/Arts.sol
+++ contracts/Arts.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
+// pragma experimental ABIEncoderV2;
 
 import "./Transaction.sol";
-import "./Art.sol";
+// import "./Art.sol";
 import "./People.sol";
 
 contract Arts {
@@ -10,20 +10,22 @@ contract Arts {
     uint[] arts;
     uint[] people;
 
-    mapping(uint => Art) artInfo;
-    mapping(uint => Transaction) transactionInfo;
+    struct Art {
+        uint artId;
+        string artName;
+        uint owner;
+    }
 
-    mapping(uint => address) artOwner;
+    mapping(uint => Art) artInfo;
+    mapping(uint => Transaction[]) transactionInfo;
+
+    mapping(uint => uint) artOwner;
 
     mapping(uint => People) peopleInfo;
 
-    mapping(address => uint) artMap;
-    
-    mapping(uint => address) artRevMap;
-
     constructor(uint[] memory _array, uint[] memory _p) public payable {
-		  arts = _array;
-		  people = _p;
+        arts = _array;
+        people = _p;
     }
 
     function getArts() public view returns(uint[] memory) {
@@ -33,8 +35,10 @@ contract Arts {
         }
         return temp;
     }
-    function getArtInfo(uint _artId) public payable returns(uint, string memory, string memory, Transaction[] memory) {
-        return (artInfo[_artId].artId(), artInfo[_artId].artName(), peopleInfo[artInfo[_artId].owner()].name(), artInfo[_artId].getHistory());
+    // function getArtInfo(uint _artId) public view returns(uint, string memory, uint, Transaction[] memory) {
+        function getArtInfo(uint _artId) public view returns(uint, string memory, uint) {
+        // return (artInfo[_artId].artId, artInfo[_artId].artName, artInfo[_artId].owner, transactionInfo[_artId]);
+        return (artInfo[_artId].artId, artInfo[_artId].artName, artInfo[_artId].owner);
     }
     function addPeople(uint _id, string memory _name) public payable {
         People p = new People(_id, _name);
@@ -42,30 +46,30 @@ contract Arts {
         peopleInfo[_id] = p;
     }
     function addArt(uint _artId, string memory name, uint owner) public payable {
-        Art a = new Art(_artId, name, owner);
+        Art memory a = Art(_artId, name, owner);
         arts.push(_artId);
         artInfo[_artId] = a;
-        artRevMap[_artId] = address(a);
+        artOwner[_artId] = owner;
     }
 
     function Time_call() public view returns (uint256){
         return now;
     }
 
-    function applyTransaction(uint _transactionId, uint oldOwner, uint newOwner, uint art, uint _price) public payable {
-        require(oldOwner != newOwner);
-        require(_price > 0);
-        require(artInfo[art].owner() == oldOwner);
+    // function applyTransaction(uint _transactionId, uint oldOwner, uint newOwner, uint art, uint _price) public payable {
+    //     require(oldOwner != newOwner);
+    //     require(_price > 0);
+    //     require(artInfo[art].owner() == oldOwner);
         
-        for(uint i = 0; i < peopleInfo[oldOwner].getArts().length; i++){
-          if(peopleInfo[oldOwner].getArts()[i] == artInfo[art].artId()) {
-            peopleInfo[oldOwner].deleteArt(i);
-          }
-        }
-        peopleInfo[newOwner].pushArt(art);
-        Transaction t = new Transaction(_transactionId, artInfo[art].artId(), artInfo[art].artName(), _price, Time_call(), oldOwner, newOwner);
-        artInfo[art].addHistory(t);
-        artInfo[art].changeOwner(newOwner);
-        transactionInfo[_transactionId] = t;
-    }
+    //     for(uint i = 0; i < peopleInfo[oldOwner].getArts().length; i++){
+    //       if(peopleInfo[oldOwner].getArts()[i] == artInfo[art].artId()) {
+    //         peopleInfo[oldOwner].deleteArt(i);
+    //       }
+    //     }
+    //     peopleInfo[newOwner].pushArt(art);
+    //     Transaction t = new Transaction(_transactionId, artInfo[art].artId(), artInfo[art].artName(), _price, Time_call(), oldOwner, newOwner);
+    //     artInfo[art].addHistory(t);
+    //     artInfo[art].changeOwner(newOwner);
+    //     transactionInfo[_transactionId] = t;
+    // }
 }
